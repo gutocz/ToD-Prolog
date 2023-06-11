0a2d9d04f8ca783dff401838f3c19e81b019f52e
lucasobelmiro <lucas.belmiro@ccc.ufcg.edu.br>
2023-06-11 18:44:25 -0300

    correção database

--- app/Modules/Database/Database2.pl
+++ app/Modules/Database/Database2.pl
@@ -1,24 +1,26 @@
-:- use_module(library(file_systems)).
 :- use_module(library(system)).
+
+directoryDatabase(Directory) :-
+    Directory = 'Modules/Database/LocalUsers/'.
 
 createToDoListDatabase(Username, ListName, ListDesc) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName], ListDir),
     atomic_list_concat([ListDir, '/', '0', ListName, '.txt'], FilePath),
     \+ exists_directory(ListDir), % Verifica se o diretório não existe
-    create_directory(ListDir),
+    make_directory(ListDir),
     open(FilePath, write, Stream),
     format(Stream, "~w~n", [ListDesc]),
     format(Stream, "~w~n", [ListName]),
     close(Stream).
 
 deleteToDoListDatabase(Username, ListName) :-
-    atomic_list_concat([directoryDatabase, Username, '/listas/', ListName], FilePath),
-    remove_directory_recursive(FilePath).
+    atomic_list_concat([directoryDatabase, Username, '/listas/', ListName], DirectoryPath),
+    delete_directory(DirectoryPath).
 
 addUserToListDatabase(Username, Creator, ListName) :-
     atomic_list_concat([directoryDatabase, Username, '/sharedWithMe'], ListDir),
     atomic_list_concat([ListDir, '/', ListName], UserList),
-    exists_directory(ListDir), % Verifica se o diretório existe
+    exists_directory(ListDir),
     (exists_file(UserList) ->
         open(UserList, append, Stream),
         format(Stream, "~w~n~w~n", [Creator, ListName]),
@@ -31,21 +33,20 @@ addUserToListDatabase(Username, Creator, ListName) :-
 
 removeUserFromListDatabase(Username, ListName) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName, '/users.txt'], FilePath),
-    exists_file(FilePath), % Verifica se o arquivo existe
+    exists_file(FilePath),
     read_file_to_string(FilePath, Contents, []),
-    split_string(Contents, "\n", "\n", Permissions),
-    delete(Permissions, Username, NewPermissions),
-    atomic_list_concat(NewPermissions, "\n", NewContents),
+    split_string(Contents, "\n", "\n", Lines),
+    exclude(=(Username), Lines, NewLines),
+    atomic_list_concat(NewLines, "\n", NewContents),
     open(FilePath, write, Stream),
     write(Stream, NewContents),
     close(Stream).
 
 getSharedListDatabase(Username, Lists) :-
     atomic_list_concat([directoryDatabase, Username, '/sharedWithMe'], FilePath),
-    exists_directory(FilePath), % Verifica se o diretório existe
+    exists_directory(FilePath),
     directory_files(FilePath, Contents),
-    include(\=(.), Contents, Filtered), % Filtra os nomes de diretórios "." e ".."
-    Lists = Filtered.
+    exclude(=(.), Contents, Lists).
 
 addTaskDatabase(Username, ListName, TaskName, TaskDesc, TaskDate, TaskPriority) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName, '/', TaskName], FilePath),
@@ -60,6 +61,7 @@ showTaskContentDatabase(Username, ListName, TaskName, Content) :-
 
 deleteTaskDatabase(Username, ListName, TaskName) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName, '/', TaskName], FilePath),
+    exists_file(FilePath),
     delete_file(FilePath).
 
 editTaskDatabase(Username, ListName, TaskName, NewData, OldData) :-
@@ -67,23 +69,20 @@ editTaskDatabase(Username, ListName, TaskName, NewData, OldData) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName, '/', TaskName, '(editado)'], NewFilePath),
     read_file_to_string(FilePath, Contents, []),
     split_string(Contents, "\n", "\n", Lines),
-    (OldData = "name" ->
-        replace_task_data(Lines, 0, NewData, NewLines)
-    ; OldData = "description" ->
-        replace_task_data(Lines, 1, NewData, NewLines)
-    ; OldData = "date" ->
-        replace_task_data(Lines, 2, NewData, NewLines)
-    ; OldData = "priority" ->
-        replace_task_data(Lines, 3, NewData, NewLines)
-    ),
+    replace_task_data(Lines, OldData, NewData, NewLines),
     atomic_list_concat(NewLines, "\n", NewContents),
     open(NewFilePath, write, Stream),
     write(Stream, NewContents),
     close(Stream).
 
-replace_task_data(Lines, Index, NewData, NewLines) :-
-    nth0(Index, Lines, OldData),
-    nth0(Index, NewLines, NewData, Lines, OldData).
+replace_task_data(Lines, "name", NewData, [NewData|Rest]) :-
+    append([_|Rest], Lines).
+replace_task_data(Lines, "description", NewData, [Head, NewData|Rest]) :-
+    append([Head|Rest], Lines).
+replace_task_data(Lines, "date", NewData, [Head1, Head2, NewData|Rest]) :-
+    append([Head1, Head2|Rest], Lines).
+replace_task_data(Lines, "priority", NewData, [Head1, Head2, Head3, NewData|Rest]) :-
+    append([Head1, Head2, Head3|Rest], Lines).
 
 ifNewTaskExists(Username, ListName, TaskName) :-
     atomic_list_concat([directoryDatabase, Username, '/listas/', ListName, '/', TaskName, '.new'], FilePath),
