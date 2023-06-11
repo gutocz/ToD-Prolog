de4cf1048d4692f17da4edf9824f95f84518bb86
Lucas Oliveira Belmiro <50140818+lucasobelmiro@users.noreply.github.com>
2023-06-11 13:30:16 -0300

    Add files via upload
    escopo todolist

---
+++ app/Modules/ToDoList/ToDoList.pl
@@ -0,0 +1,26 @@
+createToDoList(Param1, Param2, Param3, Param4) :-
+    createToDoListDatabase.
+
+deleteToDoList(Param1, Param2, Param3) :-
+    deleteToDoListDatabase.
+
+addUserToList(Param1, Param2, Param3, Param4) :- 
+    addUserToListDatabase.
+
+removeUserFromList(Param1, Param2, Param3) :-
+    removeUserFromListDatabase.
+
+getSharedList(Param1, []) :-
+    getSharedListDatabase.
+
+addTask(Param1, Param2, Param3, Param4, Param5, Param6, Param7) :-
+    addTaskDatabase.
+
+showTaskContent(Param1, Param2, Param3, []) :- 
+    showTaskContentDatabase.
+
+deleteTask(Param1, Param2, Param3, Param4) :- 
+    deleteTaskDatabase.
+
+editTask(Param1, Param2, Param3, Param4, Param5, Param6) :-
+    editTaskDatabase.
